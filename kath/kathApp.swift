a2b25e71a99eb728e63d8af552f91d5dd2057292
Faheemmalla <faheem.yousuf2004@gmail.com>
2025-10-17 10:14:45 +0530

    Initial Commit

---
+++ kath/kathApp.swift
@@ -0,0 +1,17 @@
+//
+//  kathApp.swift
+//  kath
+//
+//  Created by faheem yousuf malla on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct kathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
